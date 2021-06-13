e4236af30e4b7f46c14a1c78d01edce0730a2c17
Vadim <cocolacre@gmail.com>
2021-06-13 22:38:19 +0300

    Successfully uploaded and redirected

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length=512)
-    image = models.ImageField(upload_to = "uploads/", max_length=512)
+    image = models.ImageField(upload_to = "uploads/", max_length=512,default="1.png")
     # file will be uploaded to MEDIA_ROOT/uploads
     
 #what if we delete image file?
@@ -15,7 +15,7 @@ class Image(models.Model):
 #what if we work in Windows user filesystem from WSL...
 
 class ResizedImage(models.Model):
-    original = models.ForeignKey(Image, on_delete = models.CASCADE)
+    original = models.ForeignKey(Image, on_delete = models.CASCADE,default="1")
     name = models.CharField(max_length=512)
-    image = models.ImageField(upload_to = "uploads/", max_length=512)
+    image = models.ImageField(upload_to = "resized_uploads/", max_length=512,default="1.png")
     
