10f7e5e0404457ebedbf4cf76cb019bd652c7156
cocolacre <cocolacre@gmail.com>
2021-06-13 17:59:38 +0300

    Set timezone

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+#TODO: 4th argument: arg="test_kwarg_in_path"
 urlpatterns = [
     path("",views.index,name="index")
     ]
+#"name" kwarg is to be used in other parts of Django, e.g. in templates
