31ada664ea84e27074cfc7290710d7d4ba7b4b93
cocolacre <cocolacre@gmail.com>
2021-06-13 18:06:38 +0300

    what if-s

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_filename = models.CharField(max_length=512)
+#what if we delete image file?
+#what if filenames are duplicated?
+#what if image load fails?
+
+
+class ResizedImage(models.Model):
+    image = models.ForeignKey(Image, on_delete = models.CASCADE)
+    resized_image_filename = models.CharField(max_length=512)
--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 #TODO: 4th argument: arg="test_kwarg_in_path"
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
     ]
 #"name" kwarg is to be used in other parts of Django, e.g. in templates
